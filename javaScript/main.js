6eb6a505cf8b21a11f0d4ccefea1696491a1f5e5
saddamarbaa <saddamarbaas@gmail.com>
2021-01-10 16:11:13 +0700

    javaScript main file

---
+++ javaScript/main.js
@@ -0,0 +1,5 @@
+const gearCheck = document.getElementById("gear-check");
+const colorOption = document.getElementById("color-option");
+
+gearCheck.onclick = () => colorOption.classList.toggle("displayShow");
+// console.log(colorOption);
